a40b189570e008e9cd34604d71777fa7d52a0689
UlanovDmitry <ulanov.ktn@gmail.com>
2021-03-12 17:10:10 +0700

    .h to src

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include "config_loader.h"
 #include "db.h"
-#include <RecReg.pb.h>
 #include <iostream>
-#include <tbb/tbb.h>
-#include <thread>
+//#include <RecReg.pb.h>
+//#include <tbb/tbb.h>
+//#include <thread>
 
 using namespace std;
 
 void hello() { cout << "Hello World!" << endl; }
 
 int main() {
-  GOOGLE_PROTOBUF_VERIFY_VERSION;
+  //GOOGLE_PROTOBUF_VERIFY_VERSION;
 
   get_data();
 
-  std::cout << "TBB version: " << TBB_VERSION_MAJOR << "." << TBB_VERSION_MINOR
-            << std::endl;
+  //std::cout << "TBB version: " << TBB_VERSION_MAJOR << "." << TBB_VERSION_MINOR << std::endl;
 
-  std::thread t(hello);
-  t.join();
+  //std::thread t(hello);
+  //t.join();
 
   ConfigLoader cl;
   cl.doSmth();
